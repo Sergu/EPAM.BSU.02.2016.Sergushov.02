3894f0fea063a59df3f7f68ac26a482f0ade6115
Sergu <sergushov.1995@mail.ru>
2016-03-17 20:11:55 +0300

    task3

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task3;
+using System.Threading;
+using System.Globalization;
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+            HexFormatProvider prov = new HexFormatProvider();
+            int a = 0;
+            Console.WriteLine(string.Format(prov,"{0:H}",15));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Task3/HexFormatProvider.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Task3
+{
+    public class HexFormatProvider : IFormatProvider,ICustomFormatter
+    {
+        private IFormatProvider parent;
+        public object GetFormat(Type formatType)
+        {
+            if (formatType == typeof(ICustomFormatter))
+            {
+                return this;
+            }
+            return null;
+        }
+        public HexFormatProvider() : this(CultureInfo.CurrentCulture) { }
+        public HexFormatProvider(IFormatProvider parent)
+        {
+            this.parent = parent;
+        }
+        public string Format(string format, object arg, IFormatProvider formatProvider) 
+        {            
+            if(arg == null || format != "W")
+            {
+                return string.Format(parent, format, arg);
+            }
+            int numb = (int)arg;
+            return HexFormat(numb);
+        }
+        private string HexFormat(double numb)
+        {
+            string[] str = "0 1 2 3 4 5 6 7 8 9 A B C D E F".Split();
+            string res = "";
+            bool Minusflag = false;
+            if (numb < 0)
+            {
+                Minusflag = true;
+                numb = numb - 2 * numb;
+            }
+            int celoe = (int)Math.Round(numb, 0);
+            double ost = numb - celoe;
+
+            int o = 0;
+            while (celoe != 0)
+            {
+                res = str[(celoe % 16)] + res;
+                celoe = celoe / 16;
+            }
+            if (Minusflag)
+            {
+                res = "-" + res;
+            }
+            return res;
+        }
+    }
+}
